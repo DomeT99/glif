9b5f96198f6bdc75374344ab521c4978e31c6cdb
Domenico Tenace <domenicotenace99@gmail.com>
2025-09-24 23:03:33 +0200

    fix: link into footer

--- app/components/common/Footer.vue
+++ app/components/common/Footer.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-center">
       <p>
         Made with ❤️ by
-        <a href="https://example.com">Domenico Tenace</a>
+        <a href="https://linktr.ee/domenicotenace">Domenico Tenace</a>
       </p>
     </div>
   </footer>
